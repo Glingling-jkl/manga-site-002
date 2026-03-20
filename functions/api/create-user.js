bb29998c4fdbe571f2478add9ded46995fb77300
Glingling-jkl <vzv_yu@outlook.com>
2026-03-21 01:03:44 +0800

    Update create-user.js

--- functions/api/create-user.js
+++ functions/api/create-user.js
@@ -51,4 +51,4 @@ async function sha256(message) {
     const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-}
+                                                  }
